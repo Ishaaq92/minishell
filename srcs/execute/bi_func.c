14147d4d3120eea4795ad25fc7e9f174fe6b6c50
Ishaaq Ahmed <isahmed@c2r4s8.42london.com>
2025-04-25 14:43:50 +0100

    New file: bi_func.c is for built in functions.

---
+++ srcs/execute/bi_func.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bi_func.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isahmed <isahmed@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 18:08:06 by isahmed           #+#    #+#             */
+/*   Updated: 2025/04/24 19:12:04 by isahmed          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+// functions to build 
+// echo: NOT DONE
+// cd: NOT DONE
+// export: NOT DONE
+// unset: NOT DONE
+// exit: NOT DONE
+// env: TEST
+// pwd: TEST
+
+int	bi_cd(t_data *data)
+{
+	t_envp *lst;
+	lst = data->lst;
+}
+
+char	*bi_pwd(t_data *data)
+{
+	char	**env;	
+	int		i;
+
+	i = 0;
+	env = data->env;
+	while (env[i] != NULL && ft_strncmp(env[i], "pwd", 3) == 0)
+		i++;
+	if (env[i][4] == '=')
+		return (value_envp(env[i], "pwd"));
+	else if (env[i] == NULL)
+		return (NULL);
+}
+
+void	bi_env(t_data *data)
+{
+	char	*env;
+	int		i;
+
+	i = 0;
+	while (env[i] != NULL)
+		printf("%s", env[i++]);
+}
+
+// str must be the full string eg. 'pwd=/home/tim'
+void	bi_export(t_data *data, char *str)
+{
+	append_node(&data->head, &str, str);
+}
