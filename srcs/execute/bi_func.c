f05a68af6ccb1f0480494a5b6fdeb4b3059219ca
Ishaaq Ahmed <isahmed@c2r4s8.42london.com>
2025-04-25 14:55:37 +0100

    New file bi_func.c

--- srcs/execute/bi_func.c
+++ srcs/execute/bi_func.c
@@ -6,7 +6,7 @@
 /*   By: isahmed <isahmed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 18:08:06 by isahmed           #+#    #+#             */
-/*   Updated: 2025/04/24 19:12:04 by isahmed          ###   ########.fr       */
+/*   Updated: 2025/04/25 14:52:58 by isahmed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 // functions to build 
 // echo: NOT DONE
 // cd: NOT DONE
-// export: NOT DONE
 // unset: NOT DONE
 // exit: NOT DONE
+// export: ALMOST DONE
 // env: TEST
 // pwd: TEST
 
 int	bi_cd(t_data *data)
 {
 	t_envp *lst;
-	lst = data->lst;
+	lst = data->env_llst;
 }
 
 char	*bi_pwd(t_data *data)
@@ -33,7 +33,7 @@ char	*bi_pwd(t_data *data)
 	int		i;
 
 	i = 0;
-	env = data->env;
+	env = data->env_llst;
 	while (env[i] != NULL && ft_strncmp(env[i], "pwd", 3) == 0)
 		i++;
 	if (env[i][4] == '=')
@@ -53,7 +53,9 @@ void	bi_env(t_data *data)
 }
 
 // str must be the full string eg. 'pwd=/home/tim'
+// str can be in the form 'pwd="/home/tim"
 void	bi_export(t_data *data, char *str)
 {
+	// Quote Removal.
 	append_node(&data->head, &str, str);
 }
