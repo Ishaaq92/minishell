375e87b0b5a6a2a9ea6faa6f622ffa5b3b4bd505
avalsang <avalsang@c1r5s2.42london.com>
2025-05-02 15:13:52 +0100

    exit status implemented, doesn't update arg properly yet

--- srcs/execute/param_sub.c
+++ srcs/execute/param_sub.c
@@ -13,7 +13,8 @@
 #include "../inc/minishell.h"
 
 static char	*get_param_name(char *str);
-void	copy_latter_half_of_string(char **result, char *str);
+static void	copy_latter_half_of_string(char **result, char *str);
+static void	perform_sub(t_data *data, char **str, int i, char *param);
 
 static char	*get_param_name(char *str)
 {
@@ -26,54 +27,38 @@ static char	*get_param_name(char *str)
 }
 
 // put me in coach
-void	param_sub_2(t_data *data, char **str)
+void	param_sub(t_data *data, char **str)
 {
 	int		i;
-	char	*result;
-	char	*sub_str;
-	char	*temp;
-	char	*temp2;
 	char	*param;
 
 	i = 0;
-	result = NULL;
-	while ((*str) && (*str)[i])
+	while (*str && (*str)[i])
 	{
 		if ((*str)[i] == '$')
 		{
 			if ((*str)[i + 1] == '?')
 			{
-				printf("exit status = %i\n", data->exit_status);
+				param = ft_itoa(data->exit_status);
+				perform_sub(data, str, i, param);
 				i++;
 				continue ;
 			}
-			if (result)
-				free(result);
-			result = ft_strndup((*str), i - 1);
-			param = get_param_name((*str) + 1);
-			temp = value_envp(&data->env_llst, param);
-			temp2 = result;
-			result = ft_strjoin(temp2, temp);
-			free(temp2);
+			param = get_param_name((*str) + i + 1);
+			perform_sub(data, str, i, param);
 		}
 		i++;
 	}
-	if (result)
-	{
-		free((*str));
-		(*str) = result;
-	}
 }
 
-void	perform_sub(t_data *data, char **str, int i)
+static void	perform_sub(t_data *data, char **str, int i, char *param)
 {
 	char	*sub_value;
 	char	*result;
-	char	*param;
 	char	*temp;
 
 	result = NULL;
-	param = get_param_name((*str) + i + 1);
+	
 	sub_value = value_envp(&data->env_llst, param);
 	if (sub_value)
 	{
@@ -91,33 +76,11 @@ void	perform_sub(t_data *data, char **str, int i)
 	free(param);
 }
 
-void	copy_latter_half_of_string(char **result, char *str)
+static void	copy_latter_half_of_string(char **result, char *str)
 {
 	char	*temp;
 
 	temp = *result;
 	*result = ft_strjoin(temp, str);
 	free(temp);
-	// printf("latter half of strin = %s\n", str);
-}
-
-void	param_sub(t_data *data, char **str)
-{
-	int		i;
-
-	i = 0;
-	while (*str && (*str)[i])
-	{
-		if ((*str)[i] == '$')
-		{
-			if ((*str)[i + 1] == '?')
-			{
-				// set exit code here
-				i++;
-				continue ;
-			}
-			perform_sub(data, str, i);
-		}
-		i++;
-	}
 }
