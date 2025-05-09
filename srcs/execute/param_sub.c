09efc9aaf46b59a805d2dee101caaa2b62df38e0
avalsang <avalsang@c1r5s2.42london.com>
2025-05-09 15:51:19 +0100

    relatively hapy with how single and double quotes are treated in param_sub

--- srcs/execute/param_sub.c
+++ srcs/execute/param_sub.c
@@ -28,16 +28,23 @@ static char	*get_param_name(char *str)
 	return (ft_strndup(str, i));
 }
 
-void	skip_quotes(t_data *data, char **str, int *dquote, int *i)
+int	skip_quotes(t_data *data, char **str, int *dquote, int *i)
 {
 	if ((*str)[*i] == '\"')
+	{
 		(*dquote)++;
-	if ((*str)[*i] == '\'')
+		(*i)++;
+		return (1);
+	}
+	else if ((*str)[*i] == '\'' && (*dquote % 2) == 0)
 	{
 		(*i)++;
-		while ((*str)[(*i)] != '\'' && (*dquote % 2) == 0)
+		while ((*str)[*i] && (*str)[(*i)] != '\'')
 			(*i)++;
+		(*i)++;
+		return (1);
 	}
+	return (0);
 }
 
 // put me in coach
@@ -52,11 +59,13 @@ void	param_sub(t_data *data, char **str)
 	dquote = 0;
 	while (*str && (*str)[i])
 	{
-		skip_quotes(data, str, &dquote, &i);
+		if (skip_quotes(data, str, &dquote, &i))
+			continue ;
 		if ((*str)[i] == '$')
 		{
 			key = get_param_name((*str) + i + 1);
 			perform_sub(data, str, i, key);
+			continue ;
 		}
 		i++;
 	}
